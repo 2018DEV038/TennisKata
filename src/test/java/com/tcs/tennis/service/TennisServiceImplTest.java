f10387677a8b4cc7f20da32224d04dc4645f0008
balajiv <bala111990@gmail.com>
2018-12-24 20:21:59 +0530

    added More test scenario for calculate score description

--- src/test/java/com/tcs/tennis/service/TennisServiceImplTest.java
+++ src/test/java/com/tcs/tennis/service/TennisServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.tcs.tennis.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import com.tcs.tennis.domain.Player;
 import com.tcs.tennis.domain.TennisGame;
 import com.tcs.tennis.dto.OutputResponse;
+import com.tcs.tennis.util.Utility;
+import mockit.MockUp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,26 +48,71 @@ public class TennisServiceImplTest {
 
     @Test
     public void testToIncreasePlayerOneScoreCount() {
+        playerOneWon();
 
-        playerOne.setScore(1);
+        playerOne.setScore(0);
         playerTwo.setScore(0);
         tennisServiceImpl.getScoreDetails(tennisGame);
         assertEquals(1, playerOne.getScore());
     }
 
+
     @Test
     public void testToIncreasePlayerTwoScoreCount() {
-
+        playerTwoWon();
         playerTwo.setScore(0);
-        playerOne.setScore(1);
+        playerOne.setScore(0);
         tennisServiceImpl.getScoreDetails(tennisGame);
         assertEquals(1, playerTwo.getScore());
 
     }
 
+
+
+
+    @Test
+    public void testToVerifyFirstServiceForPlayerOneScoreZero() {
+        playerOneWon();
+        playerOne.setScore(-1);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals(0, playerOne.getScore());
+        assertEquals("LOVE - LOVE", outputResponse.getScore());
+
+    }
+
+    @Test
+    public void testToVerifyFirstServiceForPlayerTwoScoreZero() {
+        playerTwoWon();
+        playerTwo.setScore(-1);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals(0, playerTwo.getScore());
+        assertEquals("LOVE - LOVE", outputResponse.getScore());
+
+    }
+
+    @Test
+    public void testTocalculateScoreDescriptionForNull() {
+        playerOneWon();
+        playerOne.setScore(0);
+        playerTwo.setScore(0);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("FIFTEEN - LOVE", outputResponse.getScore());
+
+    }
+
+    @Test
+    public void testTocalculateScoreDescriptionForWonPlayerOne() {
+        playerOneWon();
+        playerOne.setScore(5);
+        playerTwo.setScore(4);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("YAN WON THE SET ", outputResponse.getScore());
+
+    }
+
     @Test
     public void testTocalculateScoreDescriptionForWonPlayerTwo() {
-
+        playerTwoWon();
         playerOne.setScore(4);
         playerTwo.setScore(5);
         OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
@@ -75,7 +123,7 @@ public class TennisServiceImplTest {
 
     @Test
     public void testTocalculateScoreDescriptionForDeuce() {
-
+        playerOneWon();
         playerOne.setScore(4);
         playerTwo.setScore(5);
         OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
@@ -85,11 +133,117 @@ public class TennisServiceImplTest {
 
     @Test
     public void testTocalculateScoreDescriptionForAdvantageForPlayerTwo() {
-
+        playerTwoWon();
         playerOne.setScore(7);
         playerTwo.setScore(7);
         OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
         assertEquals("ZOHA advantage ", outputResponse.getScore());
     }
 
+    @Test
+    public void testTocalculateScoreDescriptionForAdvantageForPlayerOne() {
+        playerOneWon();
+        playerOne.setScore(5);
+        playerTwo.setScore(5);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("YAN advantage ", outputResponse.getScore());
+
+    }
+
+
+
+    @Test
+    public void testToCurrentServiceWinnerForPlayerOne() {
+        playerOneWon();
+        playerOne.setScore(5);
+        playerTwo.setScore(5);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("Point Goes to YAN", outputResponse.getCurrentService());
+
+    }
+
+
+
+
+
+    @Test
+    public void testToCheckOutputResponseObjectPointDescriptionToPlayerOne() {
+        playerOneWon();
+        playerOne.setScore(3);
+        playerTwo.setScore(2);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("Point Goes to YAN", outputResponse.getCurrentService());
+
+    }
+
+
+    @Test
+    public void testToCheckOutputResponseObjectScoreLOVE() {
+        playerTwoWon();
+        playerOne.setScore(0);
+        playerTwo.setScore(0);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("LOVE - FIFTEEN", outputResponse.getScore());
+    }
+
+    @Test
+    public void testToCheckOutputResponseObjectScoreFIFTEEN () {
+        playerTwoWon();
+        playerOne.setScore(0);
+        playerTwo.setScore(0);
+        OutputResponse outputResponse =  tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("LOVE - FIFTEEN", outputResponse.getScore());
+
+    }
+
+
+    @Test
+    public void testToCheckOutputResponseObjectScoreTHIRTY () {
+        playerOneWon();
+        playerOne.setScore(1);
+        playerTwo.setScore(0);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("THIRTY - LOVE", outputResponse.getScore());
+
+
+    }
+
+    @Test
+    public void testToCheckOutputResponseObjectScoreFORTY () {
+        playerOneWon();
+        playerOne.setScore(2);
+        playerTwo.setScore(0);
+        OutputResponse outputResponse = tennisServiceImpl.getScoreDetails(tennisGame);
+        assertEquals("FORTY - LOVE", outputResponse.getScore());
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void playerOneWon(){
+        new MockUp<Utility>() {
+            @mockit.Mock
+            public boolean play() {
+                return true;
+            }
+        };
+    }
+
+    private void playerTwoWon(){
+        new MockUp<Utility>() {
+            @mockit.Mock
+            public boolean play() {
+                return false;
+            }
+        };
+    }
 }
