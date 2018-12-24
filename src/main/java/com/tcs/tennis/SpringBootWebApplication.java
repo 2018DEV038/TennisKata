54f13b318a9bbc08b017c27a69a2090954c6a708
balajiv <bala111990@gmail.com>
2018-12-24 18:38:15 +0530

    SpringBoot Application Setup

---
+++ src/main/java/com/tcs/tennis/SpringBootWebApplication.java
@@ -0,0 +1,23 @@
+package com.tcs.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * Tennis Game Configuration file 
+ * 
+ *
+ */
+@Configuration
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.tcs" })
+public class SpringBootWebApplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/tennis/SpringBootWebApplicationTest.java
@@ -0,0 +1,14 @@
+package com.tcs.tennis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SpringBootWebApplicationTest {
+
+    @Test
+    public void testMain() throws Exception {
+        SpringBootWebApplication.main(new String []{});
+    }
+}
