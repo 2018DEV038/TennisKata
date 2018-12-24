c09b22e2975488040363a3b06c971835156297e4
balajiv <bala111990@gmail.com>
2018-12-24 19:17:30 +0530

    swagger configured

--- src/main/java/com/tcs/tennis/controller/GameController.java
+++ src/main/java/com/tcs/tennis/controller/GameController.java
@@ -1,5 +1,9 @@
 package com.tcs.tennis.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,8 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tennis")
+@Api(value="Tennis Game")
 public class GameController {
 
+    /**
+     *
+     * Player will play the game and get corresponding score details for Every service
+     *
+     * @param
+     * @return
+     */
+    @ApiOperation(value = "Tennis Game for two player, Registered player have unique id to Continue the Game")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful response"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "URL not found"),
+            @ApiResponse(code = 500, message = "Service not available due to technical issues")
+    }
+    )
 
     @GetMapping(value = "/score")
     public int getGameStatus(@RequestParam(value="scoreValue") int playerScore){
